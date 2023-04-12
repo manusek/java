04c400ac23e0df30bf36cd9692a248d85052a513
Kacper Długosz <kacperszlolny@gmail.com>
2023-04-12 16:52:39 +0200

    figuryv2

--- studia/src/lab05_figury/Menu.java
+++ studia/src/lab05_figury/Menu.java
@@ -3,93 +3,121 @@ package lab05_figury;
 import java.util.Scanner;
 
 public class Menu {
-   // private int r=0;
-    public void wybor(){
-        System.out.println("Koło (1)");
-        System.out.println("Kwadrat (2)");
-        System.out.println("Prostokąt (3)");
-        System.out.println("Sześcian (4)");
-        System.out.println("Prostopadłościan (5)");
-        System.out.println("Kula (6)");
-        System.out.println("Stożek (7)");
-        System.out.println("Wyjscie (8)");
-        System.out.print("Wybierz figurę: ");
-        int i = InputInt();
-        switch (i){
-            case 1: k1();
-                break;
-            case 2: k2();
-                break;
-            case 3: k3();
-                break;
-            case 4: k4();
-                break;
-            case 5: k5();
-                break;
-            case 6: k6();
-                break;
-            case 7: k7();
-                break;
+        public void wybor () {
+            while (true) {
+                System.out.println("Koło (1)");
+                System.out.println("Kwadrat (2)");
+                System.out.println("Prostokąt (3)");
+                System.out.println("Sześcian (4)");
+                System.out.println("Prostopadłościan (5)");
+                System.out.println("Kula (6)");
+                System.out.println("Stożek (7)");
+                System.out.println("Wyjscie (8)");
+                System.out.print("Wybierz figurę: ");
+                int i = InputInt();
+                switch (i) {
+                    case 1:
+                        k1();
+                        break;
+                    case 2:
+                        k2();
+                        break;
+                    case 3:
+                        k3();
+                        break;
+                    case 4:
+                        k4();
+                        break;
+                    case 5:
+                        k5();
+                        break;
+                    case 6:
+                        k6();
+                        break;
+                    case 7:
+                        k7();
+                        break;
+                    case 8:
+                        close();
+                        break;
+                }
+                String test = " ";
+                do{
+                    System.out.print("\nCzy chcesz koontynuowac ? (t/n)");
+                    test = InputString();
+                    if((test.equals("n")))close();
+                    else if((test.equals("t")))wybor();
+                }while(!(test.equals("n")) || !(test.equals("t")));
+            }
         }
-    }
-    private int InputInt(){
+        private int InputInt () {
         Scanner wczytywanie = new Scanner(System.in);
         return wczytywanie.nextInt();
     }
-    private double InputDouble(){
+        private double InputDouble () {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextDouble();
     }
-    private void k1(){
+        public static String InputString(){
+            Scanner wczytywanie2 = new Scanner(System.in);
+            String a = wczytywanie2.nextLine();
+            return a;
+        }
+        private void k1 () {
         System.out.print("Podaj promień: ");
         double r = InputDouble();
         Kolo k = new Kolo(r);
         k.wyswietl();
     }
-    private void k2(){
+        private void k2 () {
         System.out.print("Podaj bok: ");
         double a = InputDouble();
         Kwadrat kw = new Kwadrat(a);
         kw.wyswietl();
     }
-    private void k3(){
+        private void k3 () {
         System.out.print("Podaj pierwszy bok: ");
         double a = InputDouble();
         System.out.print("Podaj drugi bok: ");
         double b = InputDouble();
-        Prostakat pr = new Prostakat(a,b);
+        Prostakat pr = new Prostakat(a, b);
         pr.wyswietl();
     }
-    private void k4(){
+        private void k4 () {
         System.out.print("Podaj bok: ");
         double a = InputDouble();
         Szescian sz = new Szescian(a);
         sz.wyswietl();
     }
-    private void k5(){
+        private void k5 () {
         System.out.print("Podaj pierwszy bok: ");
         double a = InputDouble();
         System.out.print("Podaj drugi bok: ");
         double b = InputDouble();
         System.out.print("Podaj trzeci bok: ");
         double c = InputDouble();
-        Prostopadloscian pro = new Prostopadloscian(a,b,c);
+        Prostopadloscian pro = new Prostopadloscian(a, b, c);
         pro.wyswietl();
     }
-    private void k6(){
+        private void k6 () {
         System.out.print("Podaj promień: ");
         double r = InputDouble();
         Kula ku = new Kula(r);
         ku.wyswietl();
     }
-    private void k7(){
+        private void k7 () {
         System.out.print("Podaj wysokość: ");
         double h = InputDouble();
         System.out.print("Podaj promień: ");
         double r = InputDouble();
         System.out.print("Podaj bok: ");
         double l = InputDouble();
-        Stozek st = new Stozek(h,r,l);
+        Stozek st = new Stozek(h, r, l);
         st.wyswietl();
     }
+        private void close(){
+            System.out.println("Dziekujemy :)");
+            System.exit(0);
+        }
+
 }
