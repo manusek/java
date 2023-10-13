8215eec8e9d71b78b68d9c887daad60e1c884023
Kacper Długosz <kacper.dlugosz125@gmail.com>
2023-10-13 19:18:26 +0200

    students update

--- studia/src/semestr_3_cw01/Studenci.java
+++ studia/src/semestr_3_cw01/Studenci.java
@@ -8,12 +8,16 @@ public class Studenci {
     Student student = new Student();
     public void menu(){
         while(true){
+
             System.out.println(" ");
             System.out.println("Dodaj studenta (1)");
             System.out.println("Usuń studenta (2)");
             System.out.println("Wyswietl studentów (3)");
-            System.out.println("Zakończ (4)");
+            System.out.println("Wyswietl studenta po albumie (4)");
+            System.out.println("Wyswietl studenta po albumie wiekszym niz podany (5)");
+            System.out.println("Zakończ (6)");
             System.out.print("Jakie działanie chcesz wykonać: ");
+
             int a = inputInt();
 
             switch (a){
@@ -21,15 +25,61 @@ public class Studenci {
                     addStudent();
                     break;
                 }
+
                 case 2 -> {
-                    removeStudent();
+
+                    System.out.println(" ");
+                    System.out.println("Usuwanie po imieniu i nazwisku (1)\nUsuwanie po numerze albumu (2): ");
+
+                    int choice = inputInt();
+
+                    if(choice == 1) {
+
+                        System.out.print("Podaj imie ucznia do usuniecia: ");
+                        String nameToDelete = inputString();
+
+                        System.out.print("Podaj nazwisko ucznia do usuniecia: ");
+                        String scndNameToDelete = inputString();
+
+                        removeStudent(nameToDelete, scndNameToDelete);
+
+                    }else {
+                        System.out.println("Podaj numer albumu ucznia do usuniecia");
+                        int albumNumberToDelete = inputInt();
+
+                        removeStudent(albumNumberToDelete);
+                    }
                     break;
                 }
+
                 case 3 -> {
-                    showStudents();
+                        showStudents();
                     break;
                 }
+
                 case 4 -> {
+                        System.out.println(" ");
+                        System.out.print("Podaj numer albumu:  ");
+
+                        int number = inputInt();
+
+                        showStudentsByAlbumNumber(number);
+
+                        break;
+                }
+
+                case 5 -> {
+                    System.out.println(" ");
+                    System.out.print("Podaj numer albumu: ");
+
+                    int number = inputInt();
+
+                    showStudentsByAlbumNumber2(number);
+
+                    break;
+                }
+
+                case 6 -> {
                     close();
                     break;
                 }
@@ -53,25 +103,56 @@ public class Studenci {
             list.add(new Student(name, scndName, albumNumber));
     }
 
-    public void removeStudent(){
-        int i = list.size();
-        System.out.println(" ");
-        System.out.println("Ktorego studenta chcesz usunac(1-"+i+"):");
+    public void removeStudent(String itemToRemove, String itemToRemove2){
 
-
-        //TODO zrobic usuwanie przez imie, nazwisko lub nr albumu (przeciezanie metod)
-        //list.remove(number);
+        for(int j = 0; j < list.size(); j++){
+            if(list.get(j).getName().equals(itemToRemove) && list.get(j).getScndName().equals(itemToRemove2)){
+                list.remove(j);
+            }
+        }
+            //TODO doadac obslugiwanie wyjatkow
         System.out.println("Uczeń został usunięty");
         showStudents();
     }
 
+    public void removeStudent(int itemToRemove){
 
+        for(int j = 0; j < list.size(); j++){
+            if(list.get(j).getAlbumNumber() == (itemToRemove)){
+                list.remove(j);
+            }
+        }
+        //TODO dodac obslugiwanie wyjatkow
+        System.out.println("Uczeń został usunięty");
+        showStudents();
+    }
+
+    public void showStudentsByAlbumNumber(int itemToShow){
+        for(int i = 0; i < list.size(); i++){
+            if(list.get(i).getAlbumNumber() == itemToShow)
+                System.out.println("Imie: " + list.get(i).getName() +", " + "Nazwisko: " + list.get(i).getScndName());
+        }
+    }
 
     void showStudents(){
         System.out.println(" ");
         for(int i = 0; i < list.size(); i++){
             System.out.println(list.get(i));
         }
+    }
+
+    public void showStudentsByAlbumNumber2(int itemToShow){
+        ArrayList<Student> list2 = new ArrayList<>();
+
+        for(int i = 0; i < list.size(); i++) {
+            if (list.get(i).getAlbumNumber() > itemToShow) {
+                list2.add(list.get(i));
+            }
+        }
+
+            for(int j = 0; j < list2.size(); j++){
+                System.out.println(list2.get(j));
+            }
     }
 
     public String inputString(){
